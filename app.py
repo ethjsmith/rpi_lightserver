1148a743e98c149d6a0170aa89d2acff4946eb38
urd000med <ethan@esmithy.net>
2019-01-23 15:39:05 +0000

    added controls for thermostat

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,redirect
 from flask_basicauth import BasicAuth
-import subprocess, config, os
+import subprocess, config, os, requests
+
+
 
 vars = config.config()
 v0 = '-g ' + str(vars[0])
@@ -11,8 +13,8 @@ v4 = str(vars[4])
 folder = os.path.dirname(os.path.realpath(__file__))
 app = Flask(__name__,static_url_path="",static_folder=folder)
 
-app.config['BASIC_AUTH_USERNAME'] = 'ejsmith'
-app.config['BASIC_AUTH_PASSWORD'] = 'root'
+app.config['BASIC_AUTH_USERNAME'] = 'ejsmith_user'
+app.config['BASIC_AUTH_PASSWORD'] = '@allianceWithTheBlackHole11'
 app.config['BASIC_AUTH_FORCE'] = True
 
 basic_auth = BasicAuth(app)
@@ -40,6 +42,27 @@ def l_on1():
 def l_off1():
 	subprocess.call(['/usr/local/bin/rpi-rf_send',v0,v4])
 	return redirect('/')
+
+@app.route('/heatoff')
+def h_off():
+	r = requests.get('http://192.168.0.16/off')
+	return redirect('/')
+
+@app.route('/heatlow')
+def h_low():
+	r = requests.get('http://192.168.0.16/low')
+	return redirect('/')
+
+@app.route('/heatmid')
+def h_mid():
+	r = requests.get('http://192.168.0.16/mid')
+	return redirect('/')
+
+@app.route('/heathigh')
+def h_high():
+	r = requests.get('http://192.168.0.16/high')
+	return redirect('/')
+
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port = 80)
 
