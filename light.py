64d37800185a00c2d701ddc72be4c4de86ef2191
Ethan Smith <ethan@esmithy.net>
2021-10-01 12:26:30 -0600

    rework rework rework detection ( should work now? )

--- light.py
+++ light.py
@@ -12,9 +12,14 @@ def sendRequest(): # send a request to my server to turn the light on
     r = requests.get("https://ejsmith.hopto.org/control/go", params=payload)
 def getSunsetTime():
     s = Sun(lat,lon)
-    sunsetToday = s.get_local_sunset_time()
-    sunsetToday += datetime.timedelta(days=1) # for some reason the day is off by one... so add one
+    today_really = datetime.datetime.now()
+
+    sunsetToday = s.get_sunset_time()
+    secondset = s.get_local_sunset_time(today_really)
+    sunsetToday += datetime.timedelta(days=1) # the day being off by one appears to be a known issue? it is more than a year old tho
     print(f"Sunset: {sunsetToday.strftime('%d/%m/%y %H:%M')}")
+    print(f"Another Sunset: {secondset.strftime('%d/%m/%y %H:%M')}")
+    #truth = sunsetToday.replace(tzinfo=None)
     return sunsetToday
 
 
@@ -24,15 +29,21 @@ except:
     print("Please provide a user [1] and password [2] to authticate to the webserver as the program args  :) ")
     quit()
 while True:
-    now = pytz.UTC.localize(datetime.datetime.now())
+    now = pytz.UTC.localize(datetime.datetime.utcnow())
+    #now = datetime.datetime.utcnow() # this is super garbage
+    #now = now.replace(tzinfo=None)
     if 'sunsetToday' not in locals() or now.date() > sunsetToday.date(): # if it's a new day, check when the new sunset is
         sunsetToday = getSunsetTime()
-    elapsed = now - sunsetToday
+    elapsed = sunsetToday- now # gives the amount of time between now and the sunset
+    print(f"elasped:: {elapsed}")
     if -40 <= elapsed.total_seconds() <= 40 :# if our time is within about a minute of the sunset time, turn on the light
         print("Turn on! ")
         sendRequest()
     else:
         print("Don't Turn on! ")
+    # the long list of debug functions currently
     print(f"Time: {now.strftime('%d/%m/%y %H:%M')} and Sunset Time: {sunsetToday.strftime('%d/%m/%y %H:%M')}") # additional logging for debug sake ?
+    print(f"unformatted time: {now} and sunset: {sunsetToday}")
     print(f"delta: {elapsed} and remaining seconds: {elapsed.total_seconds()}")
-    time.sleep(60)# run only once per minute
+    print(f"elapsed = {elapsed} or total seconds {elapsed.total_seconds()}")
+    time.sleep(5)# run only once per minute
